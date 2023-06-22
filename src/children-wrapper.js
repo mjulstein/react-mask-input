627c4b549f999ab3919ef5ffe14c0bf47dc680dc
Martin Joakim Ulstein <6736558+mjulstein@users.noreply.github.com>
2023-06-22 14:05:18 +0200

    Children wrapper adds a div surrounding the child element
    This is not tested, but we can't have the div inside the element.
    Alternatively it should be possible to set the classNames on it or explicitly set the display-style to content

--- src/children-wrapper.js
+++ src/children-wrapper.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const InputMaskChildrenWrapper = React.forwardRef(
   function InputMaskChildrenWrapper(props, ref) {
     const { children, ...rest } = props;
-    return <div ref={ref}>{React.cloneElement(children, rest)}</div>;
+    return <>{React.cloneElement(children, {ref, ...rest})}</>;
   }
 );
 
